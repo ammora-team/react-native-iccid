2d27942828cb469cfe5326ea19a808da87c3d916
ramonornela <ramon.ornela@gmail.com>
2019-07-07 17:37:21 -0300

    fix: typo namespace

--- android/src/main/java/com/ammora/RNIccid/RNIccidModule.java
+++ android/src/main/java/com/ammora/RNIccid/RNIccidModule.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.ammora.RNIccid;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
--- android/src/main/java/com/ammora/RNIccid/RNIccidPackage.java
+++ android/src/main/java/com/ammora/RNIccid/RNIccidPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.ammora.RNIccid;
 
 import java.util.Arrays;
 import java.util.Collections;
