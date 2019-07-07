638aa10d3a7ad2ee2008ce8616312b1217f70351
ramonornela <ramon.ornela@gmail.com>
2019-07-07 16:34:36 -0300

    fix: import FLog

--- android/src/main/java/com/reactlibrary/RNIccidModule.java
+++ android/src/main/java/com/reactlibrary/RNIccidModule.java
@@ -5,6 +5,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
+import com.facebook.common.logging.FLog;
 
 import android.content.Context;
 import android.telephony.SubscriptionInfo;
